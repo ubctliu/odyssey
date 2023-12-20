500954385fb7207330e03c32393ae05a72a64a6f
Terrence <terrence.liu@alumni.ubc.ca>
2023-12-20 03:42:53 -0800

    Added footer and refactored topnav/footer to be included in layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,6 @@ export default async function Component() {
 
   return (
     <div>
-      <TopNavBar />
       <main className="px-6 py-8">
         <section className="mb-10">
           <h2 className="text-3xl font-semibold text-gray-800 mb-6 flex justify-center">Welcome to Odyssey</h2>
@@ -24,12 +23,6 @@ export default async function Component() {
           </Link>
         </div>
           <Image src={vacation} className="mx-auto" />
-          <h2 className="text-2xl font-semibold text-gray-800 m-6 flex justify-center">Why Odyssey?</h2>
-          <p className="text-gray-600">
-            We provide a unique platform for travelers to create, and share itineraries from around the world.
-            Whether you're planning a short city break or a month-long journey across continents, Odyssey helps you
-            create the perfect itinerary.
-          </p>
         </section>
       </main>
      
