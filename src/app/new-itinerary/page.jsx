b95a655ad3aad5dbcf2cd593e90623786e73d222
markmdoza <marklmdoza@gmail.com>
2023-12-21 15:11:01 -0500

    implement itineray page

--- src/app/new-itinerary/page.jsx
+++ src/app/new-itinerary/page.jsx
@@ -1,35 +1,54 @@
 import { auth } from "@clerk/nextjs";
-import vacationimg from "../../../public/images/vacationimg.png"
-import Image from "next/image"
+import vacationimg from "../../../public/images/vacationimg.png";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 import TopNavBar from "../components/TopNavBar";
-import { form } from 'react'
-
+import { form } from "react";
 
 export default async function (Component) {
   const { userId } = auth();
-  if (!userId){
-    return redirect("/sign-in")
+  if (!userId) {
+    return redirect("/sign-in");
   }
 
   return (
     <div className="h-5/6">
-    <TopNavBar />
+      <TopNavBar />
       <main className="flex justify-between p-16 bg-gray-400 items-center border border-b-8 border-solid border-b-slate-700">
         <div className="flex flex-col justify-center items-center">
-          <h1 className="text-4xl font-bold text-white mb-4">Where are you going?</h1>
+          <h1 className="text-4xl font-bold text-white mb-4">
+            Where are you going?
+          </h1>
           <form>
-            <input className="bg-white text-black p-2 rounded-lg border border-black" name="query" placeholder="Country or city"/>
+            <input
+              className="bg-white text-black p-2 rounded-lg border border-black"
+              name="query"
+              placeholder="Country or city"
+            />
             <button type="submit"> Enter Location</button>
-          </form>      
+          </form>
           <form>
-            <input className="bg-white text-black p-2 rounded-lg border border-black" name="query" placeholder="Date Range"/>
+            <input
+              className="bg-white text-black p-2 rounded-lg border border-black"
+              name="query"
+              placeholder="Date Range"
+            />
             <button type="submit"> Enter Date Range</button>
-          </form>     
-          <a href="/" className="bg-white text-black p-2 rounded-lg border border-black hover:bg-black hover:text-white">Plan My Trip!</a>
+          </form>
+          <a
+            href="/plan"
+            className="bg-white text-black p-2 rounded-lg border border-black hover:bg-black hover:text-white"
+          >
+            Plan My Trip!
+          </a>
         </div>
-        <Image src={vacationimg} alt="vacation" className="w-1/2 border-solid border-x-orange-300 border-4" />
+        <Image
+          src={vacationimg}
+          alt="vacation"
+          className="w-1/2 border-solid border-x-orange-300 border-4"
+        />
       </main>
     </div>
-  )
+  );
 }
+
---
+++ src/app/plan/page.jsx
@@ -0,0 +1,82 @@
+import { auth } from "@clerk/nextjs";
+import vacationimg from "../../../public/images/vacationimg.png";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+import TopNavBar from "../components/TopNavBar";
+
+export default async function (Component) {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/sign-in");
+  }
+
+  return (
+    <div className="h-5/6">
+      <TopNavBar />
+      <main className="flex justify-between p-16 bg-gray-400 items-center border border-b-8 border-solid border-b-slate-700">
+        <div className="flex flex-col justify-center items-center">
+          <h3 className="text-4xl font-bold text-white mb-4">
+            Where are you staying?
+          </h3>
+          <form>
+            <label htmlFor="lodging">Lodging Options: </label>
+            <select
+              className="bg-white text-black p-2 rounded-lg border border-black"
+              name="hotel_airbnbs"
+              id="hotel_airbnbs"
+            >
+              <option value="hotel">Hotel</option>
+              <option value="airbnb">Airbnb</option>
+              <option value="other">Other</option>
+            </select>
+            <input
+              className="bg-white text-black p-2 rounded-lg border border-black"
+              placeholder="Please specify if other"
+            ></input>
+          </form>
+          <h4 className="text-4xl font-bold text-white mb-4">
+            Duration of stay
+          </h4>
+          <form className="bg-white text-black p-2 rounded-lg border border-black">
+            <label htmlFor="duration"></label>
+            <select>
+              <option value="date">1 Week</option>
+              <option value="date">2 Weeks</option>
+              <option value="date">3 Weeks</option>
+              <option value="date">4 Weeks</option>
+            </select>
+          </form>
+          <h3 className="text-4xl font-bold text-white mb-4">
+            Please describe your activities for each day during the week:
+          </h3>
+          <form>
+            <select
+              className="bg-white text-black p-2 rounded-lg border border-black"
+              name="hotel_airbnbs"
+              id="hotel_airbnbs"
+            >
+              <option value="hotel">Day 1</option>
+              <option value="airbnb">Day 2</option>
+              <option value="other">Day 3</option>
+              <option value="other">Day 4</option>
+              <option value="other">Day 5</option>
+              <option value="other">Day 6</option>
+              <option value="other">Day 7</option>
+            </select>
+            <input
+              className="bg-white text-black p-2 rounded-lg border border-black"
+              placeholder="Please specify details for food or activites"
+            ></input>
+          </form>
+
+          <a
+            href="/"
+            className="bg-white text-black p-2 rounded-lg border border-black hover:bg-black hover:text-white"
+          >
+            Share My Trip!
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
