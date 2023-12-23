431ef79833947bd602de34221123b571e3c24f51
Abdul <lhlrahman@gmail.com>
2023-12-22 22:35:58 -0500

    updated package-lock

--- src/app/components/NewItineraryModal.jsx
+++ src/app/components/NewItineraryModal.jsx
@@ -1,51 +1,56 @@
+import React, { useState } from 'react';
 import { FaTimes } from 'react-icons/fa';
-import { useState } from 'react';
-import React from 'react';
 
-export default function NewItineraryModal() {
-  const [showModal, setShowModal] = useState(true);
-  const [location, setLocation] = useState("");
+export default function NewItineraryModal({ onClose }) {
+  const [title, setTitle] = useState("");
   const [date, setDate] = useState("");
-  const closeModal = () => {
-    setShowModal(false);
-  };
+  const [description, setDescription] = useState("");
+
+  // Add any other form fields or functions as needed
 
   return (
-    showModal && (
-      <div className="fixed inset-0 flex items-center justify-center z-50">
-        <div className="bg-white rounded-lg p-6 w-4/6">
-          <div className="text-center">
-            <FaTimes className="cursor-pointer" onClick={closeModal} />
-            <h2 className="text-xl font-bold mb-4">New Itinerary</h2>
-            <form>
-              <div className="mb-4">
-                <input
-                  className="border p-2 w-full"
-                  id="title"
-                  name="title"
-                  placeholder="Title"
-                  type="text"
-                  onChange={(e) => setLocation(e.target.value)}
-                />
-              </div>
-              <div className="mb-4">
-                <input
-                  className="border p-2 w-full"
-                  id="date"
-                  name="date"
-                  placeholder="Date"
-                  type="text"
-                  onChange={(e) => setDate(e.target.value)}
-                />
-              </div>
-              {/* Additional form fields */}
-              <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">
-                Save
-              </button>
-            </form>
-          </div>
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div className="bg-white rounded-lg p-6 w-4/6">
+        <div className="text-center">
+          <FaTimes className="cursor-pointer" onClick={onClose} />
+          <h2 className="text-xl font-bold mb-4">New Itinerary</h2>
+          <form>
+            <div className="mb-4">
+              <input
+                className="border p-2 w-full"
+                id="title"
+                name="title"
+                placeholder="Title"
+                type="text"
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                className="border p-2 w-full"
+                id="date"
+                name="date"
+                placeholder="Date"
+                type="text"
+                onChange={(e) => setDate(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <textarea
+                className="border p-2 w-full"
+                id="description"
+                name="description"
+                placeholder="Description"
+                rows="4"
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">
+              Save
+            </button>
+          </form>
         </div>
       </div>
-    )
+    </div>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,24 @@
-'use client'
-import Link from "next/link";
-import Image from "next/image";
-import vacation from '../../public/images/vacation.png';
-import TitleTypeWriter from "./components/TitleTypeWriter";
-import Modal from "./components/NewItineraryModal";
 import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import TitleTypeWriter from "./components/TitleTypeWriter";
+import NewItineraryModal from "./components/NewItineraryModal"; // Assuming NewItineraryModal is a separate component
+import vacation from '../../public/images/vacation.png';
 
 export default function Component() {
   const [showModal, setShowModal] = useState(false);
 
-  const handleButtonClick = () => {
+  const openModal = () => {
     setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
   };
 
   return (
     <div>
-      {showModal && <Modal />}
+      {showModal && <NewItineraryModal onClose={closeModal} />}
       <main className="px-6 py-8">
         <section className="mb-10">
           <h2 className="text-3xl font-semibold text-gray-800 mb-6 flex justify-center">Welcome to Odyssey</h2>
@@ -24,8 +27,8 @@ export default function Component() {
           </span>
           <div className="flex justify-center items-center">
             <button
-              className="text-white bg-emerald-400 hover:text-white hover:bg-emerald-500 p-2 rounded-lg border"
-              onClick={handleButtonClick}
+              className="text-white bg-emerald-400 hover:bg-emerald-500 p-2 rounded-lg border"
+              onClick={openModal}
             >
               + Create a New Itinerary
             </button>
