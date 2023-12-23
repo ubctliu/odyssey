c521a25001aef262811a75bb03a174ec21576dbc
Abdul <lhlrahman@gmail.com>
2023-12-22 20:55:09 -0500

    Update homepage to use new modal

--- src/app/components/NewItineraryModal.jsx
+++ src/app/components/NewItineraryModal.jsx
@@ -1,36 +1,51 @@
-import { Inter_Tight } from 'next/font/google';
+import { FaTimes } from 'react-icons/fa';
+import { useState } from 'react';
 import React from 'react';
 
-const initialState = {
-  showModal: false
-}
+export default function NewItineraryModal() {
+  const [showModal, setShowModal] = useState(true);
+  const [location, setLocation] = useState("");
+  const [date, setDate] = useState("");
+  const closeModal = () => {
+    setShowModal(false);
+  };
 
-const NewItineraryModal = (props) => {
-
-  const showModal = initialState.showModal
-
-return (
-  <div className="modal">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h4> Where are you going? </h4>
+  return (
+    showModal && (
+      <div className="fixed inset-0 flex items-center justify-center z-50">
+        <div className="bg-white rounded-lg p-6 w-4/6">
+          <div className="text-center">
+            <FaTimes className="cursor-pointer" onClick={closeModal} />
+            <h2 className="text-xl font-bold mb-4">New Itinerary</h2>
+            <form>
+              <div className="mb-4">
+                <input
+                  className="border p-2 w-full"
+                  id="title"
+                  name="title"
+                  placeholder="Title"
+                  type="text"
+                  onChange={(e) => setLocation(e.target.value)}
+                />
+              </div>
+              <div className="mb-4">
+                <input
+                  className="border p-2 w-full"
+                  id="date"
+                  name="date"
+                  placeholder="Date"
+                  type="text"
+                  onChange={(e) => setDate(e.target.value)}
+                />
+              </div>
+              {/* Additional form fields */}
+              <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">
+                Save
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
-      <div className="modal-body">
-        <form>
-          <input className="bg-white text-black p-2 rounded-lg border border-black" name="query" placeholder="Country or city"/>
-          <button type="submit"> Enter Location</button>
-        </form>      
-        <form>
-          <input className="bg-white text-black p-2 rounded-lg border border-black" name="query" placeholder="Date Range"/>
-          <button type="submit"> Enter Date Range</button>
-        </form>    
-      </div>
-    </div>
-    <button> 
-      Close
-    </button>
-  </div>
-)
+    )
+  );
 }
-
-export default NewItineraryModal
