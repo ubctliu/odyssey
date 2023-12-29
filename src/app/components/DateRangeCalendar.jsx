b1fd269cabad174460bb2992a2011ce0da1417ef
Angela Zhou <a03zhou@gmail.com>
2023-12-28 19:53:59 -0500

    install packages and copy code over from old branch to DateRangeCalendar component

---
+++ src/app/components/DateRangeCalendar.jsx
@@ -0,0 +1,32 @@
+'use client'
+import React from 'react';
+import { DateRange } from 'react-date-range';
+import { useState } from 'react';
+
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+
+const DateRangeCalendar = (props) => {
+  const [openDate, setOpenDate] = useState(false)
+  const [date, setDate] = useState({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: 'selection',
+  })
+
+  const handleChange = (ranges) => {
+    setDate(ranges.selection)
+  }
+
+  return (
+    <div>
+      {openDate && <DateRange
+        ranges={[date]}
+        onChange={handleChange}
+        />
+      }
+    </div>
+  )
+}
+
+export default DateRangeCalendar
