63237d0d8f6b9a3c5d06c43f44be7945927bbcb3
Angela Zhou <a03zhou@gmail.com>
2023-12-30 13:59:53 -0500

    add imports, change fuji picture

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,11 @@
+import Image from "next/image";
+import fuji from '../../../public/images/fuji.jpg';
+
 export default function About() {
   return (
+    
     <section>
+      <Image src={fuji} alt="vacation" className="w-1/2 border-solid border-x-orange-300 border-4" />
     <h2 className="text-2xl font-semibold text-gray-800 m-6 flex justify-center">Why Odyssey?</h2>
           <p className="flex justify-center text-gray-600">
             We provide a unique platform for travelers to create, and share itineraries from around the world.
