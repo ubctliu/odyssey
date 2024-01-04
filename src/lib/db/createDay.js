3a354ffcaa58a1aa9fdd946951e713f096e862ec
Angela Zhou <a03zhou@gmail.com>
2024-01-04 17:50:54 -0500

    remove comments

--- src/lib/db/createDay.js
+++ src/lib/db/createDay.js
@@ -1,7 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-//UPDATE WITH DAY INFO
 
-// we don't need to worry about same days, as each trip will have Day 1 to Day x, correct?
 export default async function createDay({ day }) {
   const prisma = new PrismaClient();
 
