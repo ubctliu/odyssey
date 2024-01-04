61412020905fe4aaf50a83ecde5d30fee59d8bdb
markmdoza <marklmdoza@gmail.com>
2024-01-04 13:53:05 -0500

    Renamed branch and created new dashboard path

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,14 +1,13 @@
 import { auth } from "@clerk/nextjs";
-import vacationimg from "../../../public/images/vacationimg.png"
-import Image from "next/image"
+import vacationimg from "../../../public/images/vacationimg.png";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 import Link from "next/link";
-
 
 export default async function (Component) {
   const { userId } = auth();
 
-  if (!userId){
+  if (!userId) {
     return redirect("/sign-in");
   }
 
@@ -16,8 +15,12 @@ export default async function (Component) {
     <div className="h-5/6">
       <main className="flex justify-between p-16 bg-gray-400 items-center border border-b-8 border-solid border-b-slate-700">
         <div className="flex flex-col justify-center items-center">
-          <h1 className="text-4xl font-bold text-white mb-4">Welcome to Odyssey</h1>
-          <p className="text-2xl font-semibold text-white mb-4">Create and share your travel itineraries</p>
+          <h1 className="text-4xl font-bold text-white mb-4">
+            Welcome to Odyssey
+          </h1>
+          <p className="text-2xl font-semibold text-white mb-4">
+            Create and share your travel itineraries
+          </p>
           <Link
             className="text-white bg-emerald-400 hover:text-white hover:bg-emerald-500 p-2 rounded-lg border"
             href="/plan/new"
@@ -25,9 +28,12 @@ export default async function (Component) {
             + Create a New Itinerary
           </Link>
         </div>
-        <Image src={vacationimg} alt="vacation" className="w-1/2 border-solid border-x-orange-300 border-4" />
-
+        <Image
+          src={vacationimg}
+          alt="vacation"
+          className="w-1/2 border-solid border-x-orange-300 border-4"
+        />
       </main>
     </div>
-  )
+  );
 }
