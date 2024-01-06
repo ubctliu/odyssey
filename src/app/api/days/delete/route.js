18de95dfe18b189693fc93f4571b3dfeb67d2662
Angela Zhou <a03zhou@gmail.com>
2024-01-05 22:21:09 -0500

    comment out days/delete

--- src/app/api/days/delete/route.js
+++ src/app/api/days/delete/route.js
@@ -1,19 +1,19 @@
-import deleteDay from '@/lib/db/deleteDay';
-import { NextResponse } from 'next/server';
+// import deleteDay from '@/lib/db/deleteDay';
+// import { NextResponse } from 'next/server';
 
-// DELETE /api/days/delete
-// Required fields in body: { Day object } - I think?
-export async function DELETE(req) {
-  if (req.method === "DELETE") {
-    const { day } = await req.json();
-    try {
-      const deletedDay = await deleteDay(day);
+// // DELETE /api/days/delete
+// // Required fields in body: { Day object } - I think?
+// export async function DELETE(req) {
+//   if (req.method === "DELETE") {
+//     const { day } = await req.json();
+//     try {
+//       const deletedDay = await deleteDay(day);
 
-      return NextResponse.json({ status: 201 }, { data: deletedDay });
-    } catch (error) {
-      return NextResponse.json({ status: 500 }, { message: `${error}: Internal server error`});
-    }
-  } else {
-    return NextResponse.json({ status: 405 }, { message: "Method not allowed" });
-  }
-}
+//       return NextResponse.json({ status: 201 }, { data: deletedDay });
+//     } catch (error) {
+//       return NextResponse.json({ status: 500 }, { message: `${error}: Internal server error`});
+//     }
+//   } else {
+//     return NextResponse.json({ status: 405 }, { message: "Method not allowed" });
+//   }
+// }
