023281372836e93d3e4be9f0ede2c580bc89c844
Angela Zhou <a03zhou@gmail.com>
2024-01-05 22:22:57 -0500

    comment out update and new routes

--- src/app/api/days/update/route.js
+++ src/app/api/days/update/route.js
@@ -1,18 +1,18 @@
-import updateDay from '@/lib/db/updateDay';
-import { NextResponse } from 'next/server';
+// import updateDay from '@/lib/db/updateDay';
+// import { NextResponse } from 'next/server';
 
-// PUT /api/days/update
-// Required fields in body: Trip object
-export async function PUT(req) {
-  if (req.method === "PUT") {
-    const { day } = await req.json();
-    try {
-      const updatedDay = await updateDay(day);
-      return NextResponse.json({ status: 201 }, { data: updatedDay });
-    } catch (error) {
-      return NextResponse.json({ status: 500 }, { message: `${error}: Internal server error`});
-    }
-  } else {
-    return NextResponse.json({ status: 405 }, { message: "Method not allowed" });
-  }
-}
+// // PUT /api/days/update
+// // Required fields in body: Trip object
+// export async function PUT(req) {
+//   if (req.method === "PUT") {
+//     const { day } = await req.json();
+//     try {
+//       const updatedDay = await updateDay(day);
+//       return NextResponse.json({ status: 201 }, { data: updatedDay });
+//     } catch (error) {
+//       return NextResponse.json({ status: 500 }, { message: `${error}: Internal server error`});
+//     }
+//   } else {
+//     return NextResponse.json({ status: 405 }, { message: "Method not allowed" });
+//   }
+// }
