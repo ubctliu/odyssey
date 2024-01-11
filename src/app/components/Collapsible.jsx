889be234c78d1bf02e95b065f98b16c568b47407
Terrence <terrence.liu@alumni.ubc.ca>
2024-01-11 02:59:29 -0800

    Added arrow with rotating effects on click for the collapsible component

--- src/app/components/Collapsible.jsx
+++ src/app/components/Collapsible.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react';
+import { IoIosArrowDown } from "react-icons/io";
 
 const Collapsible = ({ children, title }) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isRotated, setIsRotated] = useState(false);
 
   const toggleCollapse = () => {
     setIsCollapsed(!isCollapsed);
   };
-
+  
   return (
     <div>
-      <div onClick={toggleCollapse} style={{ cursor: 'pointer', borderBottom: '1px solid #ccc' }}>
-        <h3>{title}</h3>
+      <div onClick={() => {toggleCollapse();  setIsRotated(!isRotated);}} style={{ cursor: 'pointer', borderBottom: '1px solid #ccc' }}>
+        <h3>{title} <IoIosArrowDown onClick={() => setIsRotated(!isRotated)} className={`inline-block pointer-events-auto transform transition-transform duration-300 ${isRotated ? 'rotate-180' : ''}`}/></h3>
       </div>
       {!isCollapsed && <div>{children}</div>}
     </div>
