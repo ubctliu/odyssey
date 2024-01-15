609bde33279f5237fc6e8b5c57a159d9a9a86c71
Terrence <terrence.liu@alumni.ubc.ca>
2024-01-14 18:52:08 -0800

    added unoptimized tag to check

--- src/app/components/Suggestion.jsx
+++ src/app/components/Suggestion.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 export default function Suggestion({suggestion}) {
   return (
   <div className={"flex flex-col"}>
-  <Image src={suggestion.photos?.[0].getUrl() ?? '/images/image_not_found.png'} alt={suggestion.name} width="600" height="450" className="max-w-xl max-h-96"></Image>
+  <Image src={suggestion.photos?.[0].getUrl() ?? '/images/image_not_found.png'} alt={suggestion.name} width="600" height="450" className="max-w-xl max-h-96" unoptimized></Image>
   {`${suggestion.name} ⭐${suggestion.rating ? suggestion.rating + "/5" : "Rating not available."}`}
   </div>
   );
