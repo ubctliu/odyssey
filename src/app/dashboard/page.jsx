9a8cf25f033b569a21d5efb0a556d63930841f55
Angela Zhou <a03zhou@gmail.com>
2024-01-15 23:32:21 -0500

    trying to fix useEffect error

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -10,6 +10,18 @@ import { useTripData } from "@/app/context/TripDataContext";
 import { resetTripData } from "@/lib/resetTripData";
 import { fetchTripIdByUserId } from "@/lib/api";
 
+const fetchTripInfo = async (clerkId, setUserTripData) => {
+  try {
+    const fetchedUserData = await fetchTripIdByUserId(clerkId);
+    const fetchedTripData = fetchedUserData.data.trips;
+    setUserTripData(fetchedTripData);
+    
+  } catch (error) {
+    console.error('Error:', error);
+    // return null;
+  }
+};
+
 export default function Component() {
   const [showModal, setShowModal] = useState(false);
   // const [currentIndex, setCurrentIndex] = useState(0);
@@ -19,28 +31,30 @@ export default function Component() {
   const takeoff = "/videos/planetakeoff.mp4";
 
   // Don't proceed until user data is loaded
+  // if (!currentUser.isLoaded) {
+  //   return <div>Loading...</div>;
+  // }
   if (!currentUser.isLoaded) {
     return <div>Loading...</div>;
   }
 
-  const clerkId = currentUser.user.id;
-
   useEffect(() => {
-    async function fetchTripIds(clerkId) {
-      console.log("useEffect being called")
-      try {
-        const fetchedUserData = await fetchTripIdByUserId(clerkId);
-        const fetchedTripData = fetchedUserData.data.trips;
-        setUserTripData(fetchedTripData);
-        console.log('FETCHED TRIP DATA:', fetchedTripData);
-      } catch (error) {
-        console.error('Error:', error);
-        return null;
-      }
+    // Don't proceed until user data is loaded
+    if (!currentUser.isLoaded) {
+      return;
     }
 
-    fetchTripIds(clerkId);
-  }, [clerkId]); 
+    if (!currentUser.isSignedIn) {
+      return redirect("/sign-in");
+    }
+
+  }, []);
+  
+
+  useEffect(() => {
+    // const clerkId = currentUser.user.id;
+    fetchTripInfo(currentUser.user.id, setUserTripData);
+  }, [currentUser.isLoaded]); 
 
   const imageIds = Array.from(
     { length: 3 },
@@ -63,40 +77,40 @@ export default function Component() {
     <div className="h-screen flex flex-col justify-normal items-center bg-slate-350">
       <div>
         <video
-        src={takeoff}
-        autoPlay
-        loop
-        muted
-        playsInline
-        className="top-0 left-0 w-full h-50 object-fit"
+          src={takeoff}
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="top-0 left-0 w-full h-50 object-fit"
         >
         </video>
-          </div>
-      <main className="absolute h-1/2 w-1/2 mx-auto flex p-16 items-center border border-b-6 border-solid rounded-2xl border-b-slate-100 shadow-2xl backdrop-blur-md bg-white/30">
-      <h1 className="top-0 text-4xl font-bold text-slate-900 mb-4 animate-fade-in-down pt-20">
-        {currentUser.isSignedIn
-          ? `Hello, ${currentUser.user.firstName}.`
-          : "Hello, Guest"}{" "}
-        Here are your planned trips.
-      </h1>
-        <div className="flex flex-wrap justify-center items-center space-x-6 space-y-6">
-        <Carousel>
-          {userTripData.map((trip) => {
-            return (
-              <div key={trip.id} className="flex flex-col justify-center items-center w-1/4">
-          <img
-            // src={`https://picsum.photos/id/${trip.id}/181/177.jpg`}
-            alt="Trip banner image"
-            className="border rounded-lg transition-transform duration-300 ease-in-out hover:scale-110 animate-fade-in"
-          />
-        <p className="mt-2 text-center text-black font-bold">
-          {trip.title ? trip.title : trip.location}
-        </p>
       </div>
-    );
+      <main className="absolute h-1/2 w-1/2 mx-auto flex p-16 items-center border border-b-6 border-solid rounded-2xl border-b-slate-100 shadow-2xl backdrop-blur-md bg-white/30">
+        <h1 className="top-0 text-4xl font-bold text-slate-900 mb-4 animate-fade-in-down pt-20">
+          {currentUser.isSignedIn
+            ? `Hello, ${currentUser.user.firstName}.`
+            : "Hello, Guest"}{" "}
+          Here are your planned trips.
+        </h1>
+        <div className="flex flex-wrap justify-center items-center space-x-6 space-y-6">
+          <Carousel>
+            {userTripData.map((trip) => {
+              return (
+                <div key={trip.id} className="flex flex-col justify-center items-center w-1/4">
+                <img
+                  // src={`https://picsum.photos/id/${trip.id}/181/177.jpg`}
+                  alt="Trip banner image"
+                  className="border rounded-lg transition-transform duration-300 ease-in-out hover:scale-110 animate-fade-in"
+                />
+              <p className="mt-2 text-center text-black font-bold">
+                {trip.title ? trip.title : trip.location}
+              </p>
+          </div>
+        );
   })}
 </Carousel>
-{showModal && <NewItineraryModal onClose={closeModal} />}
+  {showModal && <NewItineraryModal onClose={closeModal} />}
         <section className="mb-10">
           <span className=" font-semibold text-black mb-4 flex justify-center">
             {/* <TitleTypeWriter /> */}
