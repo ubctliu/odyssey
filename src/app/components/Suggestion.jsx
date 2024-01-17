c80afa608cff4ced5eab597358aabef16f50e765
Terrence <terrence.liu@alumni.ubc.ca>
2024-01-16 21:14:30 -0800

    Changed top/bottom padding to just bottom padding

--- src/app/components/Suggestion.jsx
+++ src/app/components/Suggestion.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Suggestion({suggestion}) {
   console.log(suggestion);
   return (
-  <div className={"flex flex-col max-w-md mx-auto bg-white rounded-md overflow-hidden shadow-md space-y-auto py-10"}>
+  <div className={"flex flex-col max-w-md mx-auto bg-white rounded-md overflow-hidden shadow-md space-y-auto pb-5"}>
   <Image src={suggestion.photos?.[0].getUrl() ?? '/images/image_not_found.png'} alt={suggestion.name} width="480" height="360" className="w-full h-96 object-cover" unoptimized></Image>
   {`${suggestion.name} - ${suggestion.vicinity} ⭐${suggestion.rating ? suggestion.rating + "/5" : "Rating not available."} `} 
   </div>
