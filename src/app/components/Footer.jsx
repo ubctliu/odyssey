69cc122006d49f474e39aeb1c172706a486258f4
Terrence <terrence.liu@alumni.ubc.ca>
2024-01-16 16:12:32 -0800

    Add white color for footer in dark mode

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,7 +5,7 @@ return (
 <footer className="bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
     <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link href="/" className="hover:underline">Odyssey™</Link>. All Rights Reserved.
     </span>
-    <span className="flex justify-center">Embark on a journey of exploration and discovery with Odyssey. Your adventure awaits!</span>
+    <span className="flex justify-center dark:text-white">Embark on a journey of exploration and discovery with Odyssey. Your adventure awaits!</span>
     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
           <Link href="/about" className="hover:underline me-4 md:me-6">About</Link>
