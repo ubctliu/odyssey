45752e7f332b5b4559c621153c220e66e6372223
Terrence <terrence.liu@alumni.ubc.ca>
2024-01-18 10:20:19 -0800

    Contact page updated for Terrence

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -14,12 +14,12 @@ export default function Contact() {
             height={150}
             width={150}
             className="rounded-full px-2 py-2"
-            src={ 'https://firebasestorage.googleapis.com/v0/b/odyssey-file-storage.appspot.com/o/upload%2F0247680c-4456-445d-b25c-2eb39369ea59?alt=media&token=24913d4b-c837-408c-90a9-d01100a3c139' }
+            src={ 'https://firebasestorage.googleapis.com/v0/b/odyssey-file-storage.appspot.com/o/upload%2F5c5cbf30-40f1-4f89-a7d9-85286cadc18c?alt=media&token=c0c9fbc8-9c01-47b7-9505-8bc9ce3040e6' }
           />
           <p className="text-sm">
           <h3 className=" text-lg font-semibold">Terrence Liu <a href={"https://github.com/ubctliu"}><FaGithub className=" inline-block hover:animate-bounce"/> </a> <a href={"https://www.linkedin.com/in/terrence-liu/"}><FaLinkedin className="inline-block hover:animate-bounce"/></a></h3>
-            Terrence's interest in programming started in high school, where he spent time learning Java to make modifications to his favorite games. 
-            He enjoys learning about new things. Reach out to him out LinkedIn if you want to work on a project together!
+            Terrence's interest in programming started in high school, where he spent time learning Java to make modifications to his favorite games. While initially working towards towards a minor in Computer Science, he ended up at Lighthouse Labs where he learned about all things web-development related.
+            Now seeking new opportunities as a Full Stack Developer, he is proficient in working with React, JavaScript/TypeScript, SQL, HTML, and CSS. He enjoys learning about new things and expanding his knowledge about all things tech-related. Reach out to him out LinkedIn if you want to work on a project together!
           </p>
         </div>
     </div>
@@ -40,7 +40,7 @@ export default function Contact() {
     </div>
     <div className="grid gap-8 grid-cols-1 p-2">
         <div className="p-10 space-y-4 flex items-center align-middle mx-auto border border-black rounded-md">
-          <Image
+          <Image  
             alt="John Doe"
             height={150}
             width={150}
