f7692a7f6628f6333b22eb9cf70fe5075b918ee1
Terrence <terrence.liu@alumni.ubc.ca>
2024-01-17 17:52:10 -0800

    Fixed the time ranges placeholder on new trip

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
 return (
-<footer className="bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+<footer className="bottom-0 mx-1 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
     <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link href="/" className="hover:underline">Odyssey™</Link>. All Rights Reserved.
     </span>
     <span className="flex justify-center dark:text-white">Embark on a journey of exploration and discovery with Odyssey. Your adventure awaits!</span>
